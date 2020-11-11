ffaf6cf13c386f8bc2999ca5625dae97e96c4a72
abdou77124 <54377037+abdou77124@users.noreply.github.com>
2020-11-11 23:20:45 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -481,7 +481,21 @@ async function asyncCall() {
 
 asyncCall();
 
+let currentAccount = null;
+ethereum
+  .request({ method: 'eth_accounts' })
+  .then(handleAccountsChanged)
+  .catch((err) => {
+    // Some unexpected error.
+    // For backwards compatibility reasons, if no accounts are available,
+    // eth_accounts will return an empty array.
+    console.error(err);
+  });
 
+// Note that this event is emitted on page load.
+// If the array of accounts is non-empty, you're already
+// connected.
+ethereum.on('accountsChanged', handleAccountsChanged);
 
 // For now, 'eth_accounts' will continue to always return an array
 function handleAccountsChanged(accounts) {
