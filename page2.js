1aa7503694232e6399ee87e71055e846c2d20613
abdou77124 <54377037+abdou77124@users.noreply.github.com>
2020-11-13 14:21:19 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -462,12 +462,13 @@ const bytecode = {
 var web3 = new Web3(Web3.givenProvider || 'wss://ropsten.infura.io/ws/v3/4908cc6db7594e58b447d272c6408a1d');
 var tokenContract =  new web3.eth.Contract(abi); 
 
-
+let accounts = [];
 
 
 
 
 document.getElementById("valider").addEventListener('click', function(){
+	
 	
 	
 	async function getAccount() {
