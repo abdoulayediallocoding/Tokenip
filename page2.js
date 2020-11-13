cf62088d0b5894870485c1a278d365370108ecc4
abdou77124 <54377037+abdou77124@users.noreply.github.com>
2020-11-13 12:49:18 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -509,19 +509,22 @@ document.getElementById("valider").addEventListener('click', function(){
       }
     });
 	
-	tokenContract.deploy({
-		data: bytecode
-		})
-	.send({
-		from:currentAccount,
-		gas: 1500000,
-		gasPrice: '30000000000000'
-		})  
-	.on('error', (error) => {
-     console.log(error.message);
-	})
-	
-	
+	ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: accounts[0],
+          to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+          value: '0x29a2241af62c0000',
+          gasPrice: '0x09184e72a000',
+          gas: '0x2710',
+        },
+      ],
+    })
+    .then((txHash) => console.log(txHash))
+    .catch((error) => console.error);
+});
 	
 		
 });
