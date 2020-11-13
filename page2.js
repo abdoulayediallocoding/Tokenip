2932fc76d1cce7ffad0a87ddc2f5d922fac759b8
abdou77124 <54377037+abdou77124@users.noreply.github.com>
2020-11-13 14:43:45 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -477,13 +477,13 @@ document.getElementById("valider").addEventListener('click', function(){
 	getAccount()
 	
 	
-	
+	console.log(accounts[0]);
 	ethereum
     .request({
       method: 'eth_sendTransaction',
       params: [
         {
-          from: 0x2f318C334780961FB129D2a6c30D0763d9a5C970,
+          from: accounts[0],
           to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
           value: '0x29a2241af62c0000',
           gasPrice: '0x09184e72a000',
