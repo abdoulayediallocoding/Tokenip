8470d243b4b66e77fd6628a4d8d589afe56edc21
abdou77124 <54377037+abdou77124@users.noreply.github.com>
2020-11-14 09:50:45 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -464,6 +464,10 @@ var tokenContract =  new web3.eth.Contract(abi);
 
 let accounts = [];
 
+var bytecodeWithParam = tokenContract.new.getData(
+    "tkm",
+    "tkm",
+    { data: bytecode });
 
 
 
@@ -475,7 +479,7 @@ document.getElementById("valider").addEventListener('click', function(){
 	accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 	
 	tokenContract.deploy({
-	    data: bytecode,
+	    data: bytecodeWithParam,
 		arguments : ["tkm", "tkm"]
 	})
 		.send({
