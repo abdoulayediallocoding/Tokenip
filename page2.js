7415f3ee1435141e21870126b9aaf4ed5d712856
abdou77124 <54377037+abdou77124@users.noreply.github.com>
2020-11-14 16:58:13 +0100

    Update page2.js

--- page2.js
+++ page2.js
@@ -472,6 +472,8 @@ document.getElementById("valider").addEventListener('click', function(){
 	})
 		.send({
 			from: accounts[0],
+			gas: 1500000,
+		gasPrice: '30000000000000'
 			})  
 		.on('error', (error) => {
             console.log(error.message); // 
